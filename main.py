df389f058cdc70b7071bf135a60a0f7674308bca
Christine Emrich <christine.emrich@web.de>
2012-10-09 11:31:14 +0200

    refactored main.py into several functions

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Created on 08.10.2012
 @author: Tine
 '''
 
-template = "<html><head><meta charset='utf-8'><title>SVN Statistics</title><style type='text/css'>%s</style></head><body>%s</body></html>"
+template = "<html><head><meta charset='utf-8'><title>SVN Statistics</title><style type='text/css'>%s</style></head><body><h1>%s</h1>%s</body></html>"
 link = '<a href="%s" class="%s">%s</a>'
 cssStyle = ''
 
@@ -19,57 +19,64 @@ def loadCss():
     cssStyle = style.read()
     style.close()
 
-def writeChartsToFile(directory, charts, fileName=None, pageList=None):
+def writeChartsToFile(directory, charts, pageList, fileName=None):
     if not fileName:
         fileName = 'overview' 
-        
-    innerHtml = '<h1>' + fileName + '</h1>'
-       
-    if pageList:
-        pageList.insert(0, 'overview')
-        innerHtml += '<nav>'
-        for page in pageList:
-            cssClass = 'active' if page == fileName else ''
-            innerHtml += link % (page + '.html', cssClass, page)
-        innerHtml += '</nav>'
+     
+    pageList.insert(0, 'overview')
+    innerHtml = '<nav>'
+    for page in pageList:
+        cssClass = 'active' if page == fileName else ''
+        innerHtml += link % (page + '.html', cssClass, page)
+    innerHtml += '</nav>'
     
     for headline, chart in charts:
         innerHtml += '<h2>' + headline + '</h2>'
         innerHtml += chart.display.Img(800, 300)
-        
        
-    html = template % (cssStyle, innerHtml)
-    
-    if not path.exists(directory):
-        makedirs(directory)
+    html = template % (cssStyle, fileName, innerHtml)
 
     htmlFile = file(path.join(directory, fileName) + '.html', 'w')
     htmlFile.write(html)
     htmlFile.close()
 
-if __name__ == '__main__':
-    
-    if len(sys.argv) < 2:
-        print "please pass the full repository url as command line argument"
-        exit(2)
-        
-    repoUrl = sys.argv[1]
+def getOverviewCharts(chartGenerator):
+    charts = []
+    charts.append(('commits by date', chartGenerator.commitsByDate()))
+    charts.append(('commits by hour', chartGenerator.commitsByHour()))
+    charts.append(('commits by weekday', chartGenerator.commitsByWeekDay()))
+    charts.append(('commits by user', chartGenerator.commitsByName()))
+    return charts
+
+def getUserCharts(chartGenerator, userName):
+    charts = []
+    charts.append(('commits by date', chartGenerator.commitsByDate(userName)))
+    charts.append(('commits by hour', chartGenerator.commitsByHour(userName)))
+    charts.append(('commits by weekday', chartGenerator.commitsByWeekDay(userName)))
+    return charts
+
+def createSats(repoUrl):
     directory = repoUrl.strip('/\\').split('/')[-1]
+    
+    if not path.exists(directory):
+        makedirs(directory)
+        
     log = SvnLog(repoUrl)
     users = log.getUserList()
     generator = ChartGenerator(log)
     loadCss()
     
-    charts = []
-    charts.append(('commits by date', generator.commitsByDate()))
-    charts.append(('commits by hour', generator.commitsByHour()))
-    charts.append(('commits by weekday', generator.commitsByWeekDay()))
-    charts.append(('commits by user', generator.commitsByName()))
-    writeChartsToFile(directory, charts, pageList=list(users))
+    overViewCharts = getOverviewCharts(generator)
+    writeChartsToFile(directory, overViewCharts, list(users))
     
     for user in users:
-        charts = []
-        charts.append(('commits by date', generator.commitsByDate(user)))
-        charts.append(('commits by hour', generator.commitsByHour(user)))
-        charts.append(('commits by weekday', generator.commitsByWeekDay(user)))
-        writeChartsToFile(directory, charts, user, list(users))
+        charts = getUserCharts(generator, user)
+        writeChartsToFile(directory, charts, list(users), user)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print "please pass the full repository url as command line argument"
+        exit(2)
+        
+    createSats(sys.argv[1])
+    
